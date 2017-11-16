5c7cc0a9b55d1b468efd660f6c591faa20369235
Malgorzata Haniszewska and Qianqian Hu <qianqian@>
2017-11-16 15:29:19 -0800

    remove all multiples of prime incremented by 1

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,11 +1,13 @@
 def prime_sifting(number)
   primary_array = (2..number).to_a()
-  prime = 2
+  primes = (2..number).to_a()
+  primes.each do |prime|
     primary_array.each do |each_number|
       if (each_number == prime)
       elsif ((each_number % prime) == 0)
         primary_array.delete(each_number)
       end
     end
+  end
   return primary_array
 end
