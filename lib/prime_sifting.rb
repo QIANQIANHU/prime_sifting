d55443e2ba8ec1d9f847768e35b0e72d0a1c6210
Malgorzata Haniszewska and Qianqian Hu <qianqian@>
2017-11-16 14:20:06 -0800

    remove all multiples of prime equal 2 from the list

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,3 +1,13 @@
 def prime_sifting(number)
-  (2..number).to_a()
+  primary_array = (2..number).to_a()
+  prime = 2
+  prime_array = []
+  primary_array.each do |each_number|
+    if each_number == prime
+      prime_array.push(each_number)
+    elsif (each_number%prime) != 0
+      prime_array.push(each_number)
+    end
+  end
+  return prime_array
 end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -6,4 +6,8 @@ describe('prime_sifting') do
   it("Create a list of numbers from 2 to the given number") do
   expect(prime_sifting(3)).to(eq([2,3]))
   end
+  it("remove all multiples of prime equal 2 from the list") do
+  expect(prime_sifting(5)).to(eq([2,3,5]))
+  end
+
 end
